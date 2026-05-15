f210fd81ab852a694250c70710e4fadc6a5ad775
johnivanpuayap <johnivanpuayap@gmail.com>
2026-05-15 23:44:32 +0800

    refactor(data): extend StorageRepository with uploadBytes

--- lib/data/repositories/firebase/firebase_storage_repository.dart
+++ lib/data/repositories/firebase/firebase_storage_repository.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:unshelf_buyer/data/repositories/storage_repository.dart';
 
@@ -10,5 +11,12 @@ class FirebaseStorageRepository implements StorageRepository {
   Future<List<String>> listDownloadUrls(String path) async {
     final ListResult result = await _storage.ref(path).listAll();
     return Future.wait(result.items.map((ref) => ref.getDownloadURL()));
+  }
+
+  @override
+  Future<String> uploadBytes({required String path, required Uint8List bytes}) async {
+    final ref = _storage.ref(path);
+    await ref.putData(bytes);
+    return ref.getDownloadURL();
   }
 }
